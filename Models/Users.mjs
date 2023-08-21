b6872d23b8937896fb3b28325a1c0368da466fd6
Alizee <bocianalizee@gmail.com>
2023-08-21 03:21:16 +0200

    push after copy/past from tuto mongo db. The code doesn't work like this. Thats normal

---
+++ Models/Users.mjs
@@ -0,0 +1,19 @@
+//import mongoose from "mongoose"
+
+//const usersSchema = new mongoose.Schema({
+//    username : {
+//        first : {
+//            type : String,
+//            required : true,
+//        },
+//        last : {
+//            type : String,
+//            required : true,
+//        }
+//    }, 
+//    email : {
+//        type : String,
+//        required : true,
+//    },
+//    adress
+//})
--- index.mjs
+++ index.mjs
@@ -6,6 +6,7 @@ dotenv.config();
 
 const username_mongo = process.env.username_mongo
 const password_mongo = process.env.mongo_password
+
 mongoose.connect(`mongodb+srv://${username_mongo}:${password_mongo}@eshop.yo4pqlj.mongodb.net/?retryWrites=true&w=majority`, {
 useNewUrlParser: true,
 useUnifiedTopology: true
@@ -14,6 +15,52 @@ console.log('MongoDB Atlas connected!');
 }).catch(err => {
 console.log(err);
 });
+
+import { MongoClient } from "mongodb";
+ 
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = `mongodb+srv://${username_mongo}:${password_mongo}@eshop.yo4pqlj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(url);
+ 
+ // The database to use
+ const dbName = "eshop";
+                      
+ async function run() {
+    try {
+         await client.connect();
+         console.log("Connected correctly to server");
+         const db = client.db(dbName);
+
+         // Use the collection "people"
+         const col = db.collection("users");
+
+         // Construct a document                                                                                                                                                              
+         let personDocument = {
+             "name": { "first": "Alan", "last": "Turing" },
+             "birth": new Date(1912, 5, 23), // May 23, 1912                                                                                                                                 
+             "death": new Date(1954, 5, 7),  // May 7, 1954                                                                                                                                  
+             "contribs": [ "Turing machine", "Turing test", "Turingery" ],
+             "views": 1250000
+         }
+
+         // Insert a single document, wait for promise so we can read it back
+         const p = await col.insertOne(personDocument);
+         // Find one document
+         const myDoc = await col.findOne();
+         // Print to the console
+         console.log(myDoc);
+
+        } catch (err) {
+         console.log(err.stack);
+     }
+ 
+     finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
+
 
 
 
